a7d76081b947a889b2f8b65c99525a24edd72de4
Le-Vu Tran <vu.tran@employmenthero.com>
2022-09-11 22:07:55 +0700

    Initial release

---
+++ lib/assistant.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "assistant/version"
+
+module Assistant
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/assistant/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Assistant
+  VERSION = '0.1.0'
+end
