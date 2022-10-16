859a7294f35162e94a3be321576794f6523f0e53
Le-Vu Tran <vu.tran@employmenthero.com>
2022-10-17 00:44:41 +0700

    Enhance publish script

--- bin/publish.rb
+++ bin/publish.rb
@@ -1,4 +1,7 @@
 require_relative '../lib/assistant/version'
 
+system('gem install gemfury')
 system('gem build assistant.gemspec')
-system("curl -F package=@assistant-#{Assistant::VERSION}.gem https://#{ENV["GEMFURY_TOKEN"]}@push.fury.io/#{ENV["GEMFURY_REPOSITORY"]}/")
+
+system("fury yank assistant -v #{Assistant::VERSION} --api-token=#{ENV["GEMFURY_FULL_ACCESS_TOKEN"]}")
+system("fury push assistant-#{Assistant::VERSION}.gem --api-token=#{ENV["GEMFURY_FULL_ACCESS_TOKEN"]}")
