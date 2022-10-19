889366bf6ba7f040151d4704334be0736c3efaba
Le-Vu Tran <vu.tran@employmenthero.com>
2022-10-19 15:09:28 +0700

    Fix error

--- lib/assistant/commands/linux/tweak.rb
+++ lib/assistant/commands/linux/tweak.rb
@@ -4,11 +4,15 @@ module Assistant
   module Commands
     module Linux
       class Tweak < Dry::CLI::Command
+        include Dry::Monads[:result]
+
         desc 'Linux tweaks that make your life better'
 
         def call(**)
           Assistant::Executor.instance.with_spinner(title: 'Deploying tweaks') do
             Assistant::Tweaks.fix_postman_openssl
+
+            Success('done')
           end
         end
       end
