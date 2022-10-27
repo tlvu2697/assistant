59f24cd3f6ef5ae34d1fe0482115eabb64cbe8ae
Le-Vu Tran <vu.tran@employmenthero.com>
2022-10-27 11:49:00 +0700

    Update test script

--- bin/test.rb
+++ bin/test.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/assistant/version'
 
+system('gem uninstall -ax assistant')
 system('gem build assistant.gemspec')
 system("gem install ./assistant-#{Assistant::VERSION}.gem")
 system("rm ./assistant-#{Assistant::VERSION}.gem")
